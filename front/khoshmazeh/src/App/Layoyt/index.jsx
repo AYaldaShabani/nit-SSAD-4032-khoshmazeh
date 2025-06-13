d490bf1315d3477b856aa0be83b3e737ce137330
AYaldaShabani <yalda.shabani27@gmail.com>
2025-06-13 15:45:14 +0330

    update layout

--- front/khoshmazeh/src/App/Layoyt/index.jsx
+++ front/khoshmazeh/src/App/Layoyt/index.jsx
@@ -2,15 +2,17 @@ import Footer from "../../Components/Commen/Footer";
 import Header from "../../Components/Commen/Header";
 import { Outlet } from "react-router-dom";
 
-const Layout = () => { 
-    return (
-        <div>
-            <Header />
-            <Outlet />
-            <Footer />
-        </div>
-    )
+const Layout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
 
-}
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
